530b0b86f523872c5b6910ffd9e87b6ea6e5c889
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2024-05-11 19:12:54 -0500

    Happy with this structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-//import HelloWorld from './components/HelloWorld.vue'
-import Search from './components/Search.vue'
+import Search from './components/Search/Search.vue'
 </script>
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <!-- <HelloWorld msg="You did it, dude." /> -->
+      
       <Search />
 
       <nav>

a7a772285b6a81d39b5ea2cea19d45144e4e89fd
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2024-05-11 21:45:00 -0500

    lighter color

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,18 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
+  <main class="welcome">
+    <div class="inside"></div>
     <TheWelcome />
   </main>
 </template>
+
+<style lang="scss">
+main.welcome {
+  color: #626262;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 40px;
+}
+</style>
